2d43056e242c284b3d849b6bea04ff7e8c24872e
Pascal Cremer <b00gizm@gmail.com>
2016-05-05 20:06:44 +0200

    Removes API group
    We'll handle everything over Websockets for now

--- lib/api/default.go
+++
@@ -1,9 +0,0 @@
-package api
-
-import "github.com/gin-gonic/gin"
-
-func HelloApi(c *gin.Context) {
-	c.JSON(200, gin.H{
-		"message": "hello world",
-	})
-}
--- router.go
+++ router.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
-	"k8-rolling-demo/lib/api"
 	"net/http"
 )
 
@@ -11,9 +10,7 @@ var router *gin.Engine
 func init() {
 	router = gin.Default()
 
-	apiGroup := router.Group("/api")
 	{
-		apiGroup.GET("/", api.HelloApi)
 	}
 
 	// main html
